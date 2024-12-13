798f0d8db4dc6cb83a07d2b6cc2ba203683c8edc
Tomas R <rtomas@gmail.com>
2024-12-12 22:54:26 -0300

    ssr true

--- config/wagmi.ts
+++ config/wagmi.ts
@@ -9,6 +9,7 @@ export const projectId = "b56e18d47c72ab683b10814fe9495694"
 export const networks: [AppKitNetwork, ...AppKitNetwork[]] = [mainnet, polygon, base];
 
 export const wagmiAdapter = new WagmiAdapter({
+  ssr: true,
   networks: networks,
   projectId
 })
